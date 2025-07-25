f0c87ee2d4a77e0c7637c0d29add5539b9ac8be2
raxkumar <c.raxkumar@gmail.com>
2025-07-24 20:47:34 -0500

    removed required_version

--- versions.tf
+++ versions.tf
@@ -1,8 +1,5 @@
 terraform {
-  # required_version = ">= 0.12.0, <= 1.12.2"
-  required_version = ">= 1.3.0"
-
-
+  # required_version = ">= 1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
